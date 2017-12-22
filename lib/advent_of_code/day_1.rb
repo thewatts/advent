1eac3206974d39a96d7c9e12ad2cfcf2e5d775d0
Nathaniel Watts <reg@nathanielwatts.com>
2017-12-22 15:54:06 -0500

    Refactor Day1

--- lib/advent_of_code/day_1.rb
+++ lib/advent_of_code/day_1.rb
@@ -3,40 +3,37 @@ module AdventOfCode
     extend self
 
     def from_next_item(input)
-      digits = input.to_s.strip.split("").map(&:to_i)
-
-      digits
-        .each_with_object([])
-        .with_index do |(digit, keepers), index|
-          keepers << digit if keep_from_next?(digit, index, digits)
-        end.reduce(&:+) || 0
+      sum_digits_from_input(input, from_next: true)
     end
 
     def from_half(input)
-      digits = input.to_s.strip.split("").map(&:to_i)
-
-      digits
-        .each_with_object([])
-        .with_index do |(digit, keepers), index|
-          keepers << digit if keep_from_half?(digit, index, digits)
-        end.reduce(&:+) || 0
+      sum_digits_from_input(input, from_half: true)
     end
 
     private
 
-    def keep_from_next?(digit, index, digits)
-      candidate = digits[index + 1]
-
-      digit == candidate || (candidate.nil? && digit == digits.first)
-    end
-
     def keep_from_half?(digit, index, digits)
-      count = digits.count
-      steps = count / 2
-
-      candidate = digits.rotate(index).rotate(steps).first
+      steps     = digits.count / 2
+      candidate = digits.rotate(index + steps).first
 
       digit == candidate
+    end
+
+    def keep_from_next?(digit, index, digits)
+      candidate = digits.rotate(index + 1).first
+
+      digit == candidate
+    end
+
+    def sum_digits_from_input(input, from_half: false, from_next: false)
+      digits = input.to_s.strip.split("").map(&:to_i)
+
+      digits
+        .each_with_object([])
+        .with_index do |(digit, keepers), index|
+          keepers << digit if from_half && keep_from_half?(digit, index, digits)
+          keepers << digit if from_next && keep_from_next?(digit, index, digits)
+        end.reduce(&:+) || 0
     end
   end
 end
