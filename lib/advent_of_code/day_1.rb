c5e3b136dd0c40e1eb1aaaa470bfd11ed0371e79
Nathaniel Watts <reg@nathanielwatts.com>
2017-12-22 15:48:46 -0500

    Solve Day1 #2

--- lib/advent_of_code/day_1.rb
+++ lib/advent_of_code/day_1.rb
@@ -2,22 +2,41 @@ module AdventOfCode
   module Day1
     extend self
 
-    def call(input)
+    def from_next_item(input)
       digits = input.to_s.strip.split("").map(&:to_i)
 
       digits
         .each_with_object([])
         .with_index do |(digit, keepers), index|
-          keepers << digit if keep?(digit, index, digits)
+          keepers << digit if keep_from_next?(digit, index, digits)
+        end.reduce(&:+) || 0
+    end
+
+    def from_half(input)
+      digits = input.to_s.strip.split("").map(&:to_i)
+
+      digits
+        .each_with_object([])
+        .with_index do |(digit, keepers), index|
+          keepers << digit if keep_from_half?(digit, index, digits)
         end.reduce(&:+) || 0
     end
 
     private
 
-    def keep?(digit, index, digits)
+    def keep_from_next?(digit, index, digits)
       candidate = digits[index + 1]
 
       digit == candidate || (candidate.nil? && digit == digits.first)
+    end
+
+    def keep_from_half?(digit, index, digits)
+      count = digits.count
+      steps = count / 2
+
+      candidate = digits.rotate(index).rotate(steps).first
+
+      digit == candidate
     end
   end
 end
